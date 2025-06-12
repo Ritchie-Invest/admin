40a1539da875d76e5ddbe5d3611ce11bf3cdb120
MisterAzix <flizi@live.fr>
2025-06-12 11:46:03 +0200

    feat: adding default dashboard layout ✨

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import { IconDatabase, IconInnerShadowTop } from '@tabler/icons-react';
+
+import { NavContent } from '@/components/nav-content.tsx';
+import { NavUser } from '@/components/nav-user';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar';
+
+const data = {
+  user: {
+    name: 'Ritchie',
+    email: 'admin@ritchie-invest.com',
+    avatar: '/avatars/shadcn.jpg',
+  },
+  courses: [
+    {
+      name: 'Chapters',
+      url: '/#',
+      icon: IconDatabase,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <a href="#">
+                <IconInnerShadowTop className="!size-5" />
+                <span className="text-base font-semibold">Ritchie CRM</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavContent items={data.courses} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
