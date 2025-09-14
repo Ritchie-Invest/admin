c191e55d963d3dfc392699558e7cfa395468a1df
MisterAzix <flizi@live.fr>
2025-09-15 00:51:27 +0200

    feat: rename CRM into ADMIN cause for now we do not manage any user ✏️

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -49,7 +49,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             >
               <a href="#">
                 <IconInnerShadowTop className="!size-5" />
-                <span className="text-base font-semibold">Ritchie CRM</span>
+                <span className="text-base font-semibold">Ritchie ADMIN</span>
               </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
